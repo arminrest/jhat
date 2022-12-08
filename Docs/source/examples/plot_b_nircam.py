f5bab64ffaec2316c5b817d581b84ec8a3182071
Justin Pierel <jpierel@stsci.edu>
2022-12-08 15:41:56 -0500

    docs

---
+++ Docs/source/examples/plot_b_nircam.py
@@ -0,0 +1,14 @@
+"""
+===========
+JWST NIRCam
+===========
+
+Aligning JWST NIRCam images with JHAT.
+"""
+		
+###############################################################
+# Explain here what this example does
+   
+import jhat
+
+print('Hello World!')
