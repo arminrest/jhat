66e49a1d1be30d7b305ec36a117ef8f2f305db5a
Justin Pierel <jpierel@stsci.edu>
2022-12-09 13:21:43 -0500

    figure appear

--- Docs/source/_examples/plot_c_hst.py
+++ Docs/source/_examples/plot_c_hst.py
@@ -61,7 +61,7 @@ from jhat import hst_photclass,st_wcs_align
 #                       norm=norm1,cmap='gray')
 x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
-
+fig = plt.figure()
 plt.plot(x, y)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\sin(x)$')
