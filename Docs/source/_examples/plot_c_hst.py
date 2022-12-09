34862ed8179b8a00e7b5d59aea34b919ecd28e52
Justin Pierel <jpierel@stsci.edu>
2022-12-09 12:11:59 -0500

    updated example

--- Docs/source/_examples/plot_c_hst.py
+++ Docs/source/_examples/plot_c_hst.py
@@ -11,21 +11,14 @@ Aligning HST images with JHAT.
 # alignment methods are used.
    
 
-import sys,jwst,os,glob
-#sys.path.append('../../../')
+import sys,os,glob
 from astropy.io import fits
-from astropy.table import Table,vstack
-
-#from jwst_wcs_align import hst_wcs_align
+from astropy.table import Table
 import numpy as np
-from astropy.coordinates import SkyCoord
-import astropy.units as u
-from astropy import wcs
-from astropy.wcs.utils import skycoord_to_pixel
 import matplotlib.pyplot as plt
 from astroquery.mast import Observations
-from astropy.visualization import (simple_norm, MinMaxInterval,
-                                   LinearStretch)
+from astropy.visualization import (simple_norm,LinearStretch)
+
 import jhat
 from jhat import hst_photclass,st_wcs_align
 
@@ -33,7 +26,9 @@ from jhat import hst_photclass,st_wcs_align
 ####################################################################
 # **Download some Data**
 #
-# For this example we j
+# For this example we download 2 HST FLT images from MAST. They're
+# the same filter and same field, just separated in time. Note that 
+# the code will also work for drizzled images.
 
 obs_table = Observations.query_criteria(obs_id='hst_16264_13_wfc3_ir_f160w_iebc13')
 obs_table1 = obs_table[obs_table['filters']=='F160W']
@@ -51,6 +46,11 @@ data_products_by_obs = data_products_by_obs[data_products_by_obs['calib_level']=
 data_products_by_obs = data_products_by_obs[data_products_by_obs['productSubGroupDescription']=='FLT'][0]
 Observations.download_products(data_products_by_obs,extension='fits')
 
+####################################################################
+# **Create a Photometric Catalog for Relative Alignment**
+# We choose one of the images to be the reference image, and then
+# create a catalog that we will use to align the other image. 
+
 files = glob.glob('mastDownload/HST/*/*flt.fits')
 ref_image = files[0]
 align_image = files[1]
@@ -62,19 +62,20 @@ ax = fig.gca()
 ax.imshow(ref_data, origin='lower',
                        #interval=MinMaxInterval(),
                        norm=norm1,cmap='gray')
-#plt.show()
-
-
-
 
 hst_phot = hst_photclass(psf_fwhm=1.8,aperture_radius=5)
 hst_phot.run_phot(imagename=ref_image,photfilename='auto',overwrite=True)
+ref_catname = ref_image.replace('.fits','.phot.txt') # the default
+refcat = Table.read(ref_catname,format='ascii')
+print(refcat)
+
+####################################################################
+# **Align the second image**
+# 
 
 wcs_align = st_wcs_align()
 wcs_align.outdir = 'mastDownload'
 
-ref_catname = ref_image.replace('.fits','.phot.txt')
-refcat = Table.read(ref_catname,format='ascii')
 
 wcs_align.run_all(align_image,
           refcat_racol='ra',
@@ -85,7 +86,7 @@ wcs_align.run_all(align_image,
           #Nbright=1000,
           #Nbright4match=1000,
           d2d_max=.12,
-          showplots=1,
+          showplots=2,
           #xshift=xshift,
           #yshift=yshift,
           #refcat_maincolor='gaia_g_rp',
@@ -98,5 +99,3 @@ wcs_align.run_all(align_image,
                       SNR_min= 3,
                       dmag_max=1.0,
                       objmag_lim =(14,24))
-
-#plt.show()
