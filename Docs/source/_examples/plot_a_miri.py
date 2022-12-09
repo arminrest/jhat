1bb4b1a1e859c01aadb5c4073e6da7aaf1f43361
Justin Pierel <jpierel@stsci.edu>
2022-12-09 13:50:54 -0500

    miri

--- Docs/source/_examples/plot_a_miri.py
+++ Docs/source/_examples/plot_a_miri.py
@@ -11,6 +11,16 @@ Aligning JWST MIRI images with JHAT.
    
 import numpy as np
 import matplotlib.pyplot as plt
+import sys,os,glob
+from astropy.io import fits
+from astropy.table import Table
+import numpy as np
+import matplotlib.pyplot as plt
+from astroquery.mast import Observations
+from astropy.visualization import (simple_norm,LinearStretch)
+
+import jhat
+from jhat import hst_photclass,st_wcs_align
 
 x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
