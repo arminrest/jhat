30978ab7377d6dec44deed09229d5fbaa81bdfed
Justin Pierel <jpierel@stsci.edu>
2025-03-05 13:12:44 -0500

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os,glob,warnings,sys,fnmatch,subprocess
 from setuptools.command.test import test as TestCommand
 from distutils.core import setup
-import numpy.distutils.misc_util
+#import numpy.distutils.misc_util
 
 
 if sys.version_info < (3,0):
@@ -18,29 +18,29 @@ class jhattest(TestCommand):
 
 AUTHOR = 'Armin Rest & Justin Pierel'
 AUTHOR_EMAIL = 'arest@stsci.edu'
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 LICENSE = ''
 URL = ''
 
 
 
-def recursive_glob(basedir, pattern):
-    matches = []
-    for root, dirnames, filenames in os.walk(basedir):
-        for filename in fnmatch.filter(filenames, pattern):
-            matches.append(os.path.join(root, filename))
-    return matches
+#def recursive_glob(basedir, pattern):
+#    matches = []
+#    for root, dirnames, filenames in os.walk(basedir):
+#        for filename in fnmatch.filter(filenames, pattern):
+#            matches.append(os.path.join(root, filename))
+#    return matches
 
 PACKAGENAME='jhat'
 
 
 # Add the project-global data
-data_files = []
-for dataFolderName in ['[]']:
-  pkgdatadir = os.path.join(PACKAGENAME, dataFolderName)
-  data_files.extend(recursive_glob(pkgdatadir, '*'))
+#data_files = []
+#for dataFolderName in ['[]']:
+#  pkgdatadir = os.path.join(PACKAGENAME, dataFolderName)
+#  data_files.extend(recursive_glob(pkgdatadir, '*'))
 
-data_files = [f[len(PACKAGENAME)+1:] for f in data_files]
+#data_files = [f[len(PACKAGENAME)+1:] for f in data_files]
 
 
 setup(
@@ -56,8 +56,8 @@ setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license=LICENSE,
-    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
-    package_data={PACKAGENAME:data_files},
+    #include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+    #package_data={PACKAGENAME:data_files},
     include_package_data=True
 )
 
